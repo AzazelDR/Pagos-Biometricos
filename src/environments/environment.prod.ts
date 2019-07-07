2e253c4743b7eda9ca0637221b6a2c8efe293bf5
AzazelDR <49660378+AzazelDR@users.noreply.github.com>
2019-07-07 16:12:49 -0600

    Initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyB9SovlS9R-N7lpOl-AKIxrFY1DQ3vfnGI',
+    authDomain: 'login-9c25e.firebaseapp.com',
+    databaseURL: 'https://login-9c25e.firebaseio.com',
+    projectId: 'login-9c25e',
+    storageBucket: 'login-9c25e.appspot.com',
+    messagingSenderId: '128125679676'
+  }
+};
